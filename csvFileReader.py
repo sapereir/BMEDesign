520b972c855e939f8793aa0518540ca85983b3bd
Mia Han <miha08@Mias-MacBook-Pro-4.local>
2020-01-29 22:01:52 -0500

    converted all variables into ints besides time and termination (boolean)

--- csvFileReader.py
+++ csvFileReader.py
@@ -3,21 +3,34 @@ import sys
 from math import log
 import csv
 import re
+import torch
 
 # if __name__ == '__main__':
 #     input = sys.argv[1]
 #     print("The input file is: %s" % (input)) 
 
+contrasts=dict()
+contrastCount = 0
+catheters = dict()
+catheterCount = 0
+placements = dict()
+placementCount = 0
+protocols = dict()
+protocolCount = 0
+transients = dict()
+transientCount = 0
+
 class injectionInfo:
     def __init__(self, entry):
+        global contrastCount,catheterCount, placementCount, protocolCount, transientCount
         self.injectorSN = entry[0]
-        self.contrast = entry[1]
-        self.catheter = entry[2]
-        self.placement = entry[3]
-        self.protocol = entry[4]
+        self.contrast,contrastCount = convertIntoBinary(contrasts, contrastCount,entry[1])
+        self.catheter,catheterCount =convertIntoBinary(catheters, catheterCount,entry[2])
+        self.placement, placementCount = convertIntoBinary(placements, placementCount, entry[3])
+        self.protocol, protocolCount = convertIntoBinary(protocols, protocolCount, entry[4])
         self.injectionDate = entry[6]
         self.termination = self.setTermination(entry[7])
-        self.transients = entry[8]
+        self.transient, transientCount = convertIntoBinary(transients, transientCount, entry[8])
         self.pressureLimit = entry[9]
         self.time, self.pressure, self.rate = entry[10], entry[11], entry[12]
     
@@ -27,9 +40,12 @@ class injectionInfo:
         else:
             return True
     
+    def getMaxPressure(self):
+        return max(self.pressure)
 
 def readFile(path):
     lines = list()
+    ret = list()
     with open(path) as file:
         csv_reader = file.readlines()
         line_count = 0
@@ -42,20 +58,29 @@ def readFile(path):
                 chunk = list(map(float, chunk)) 
                 row.append(chunk)
             lines.append(row)
-    return lines
+            obj = injectionInfo(row)
+            ret.append(obj)
+    return lines, ret
 
-lines = readFile("SN-SRMC-CT-1.txt")
 
-#returns an array of injectionInfo objects created from each entry in the text file
-def createObjects(lines):
-    ret = []
-    for l in lines:
-        obj = injectionInfo(l)
-        ret.append(obj)
-    return ret
+def convertIntoBinary(dic, count, var):
+    if var not in dic.keys():
+        dic[var] = count+1
+        count += 1
+    return dic[var], count
 
-objs = createObjects(lines)
-print(objs[4].rate)
+def printInjectionInfo(info):
+    print(str(info.injectorSN) + " " + str(info.contrast) + " " + str(info.catheter) + " " + str(info.placement) + " " + str(info.protocol) + " " + str(info.injectionDate) + " " +str(info.termination) + " " + str(info.transient) + " "+ str(info.pressureLimit) + " " + str(info.time))
+
+
+lines,parsed = readFile("SN-SRMC-CT-1.txt")
+
+print(contrasts)
+print(transients)
+print(lines[1])
+printInjectionInfo(parsed[1])
+
+
         
         
 
