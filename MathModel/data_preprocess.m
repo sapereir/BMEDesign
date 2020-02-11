f2b19f99d3372d700df3d84b4a232c41972ec667
Owner <Owner@DESKTOP-ECE29PM.wifi.cmu.edu>
2020-02-11 15:56:24 -0500

    visualized derivative for truncation insight

--- MathModel/data_preprocess.m
+++ MathModel/data_preprocess.m
@@ -1,4 +1,5 @@
 %% Data Preprocessing 
+% By Shayan "Ricky" Gupta (shayang)
 
 %% Script Description
 % In this script, the raw flow and pressure data will be prepocessed by (1)
@@ -14,38 +15,42 @@ time = [0.000, 0.355, 0.591, 0.877, 1.230, 1.528, 1.878, 2.174, 2.569, 2.880, 3.
 pressure = [0, 0, 69, 145, 234, 296, 496, 600, 814, 903, 993, 1076, 1165, 1207, 1276, 1282, 1317, 1317, 1372, 1379, 1393, 1400, 1413, 1434, 1400, 1427, 1448, 1420, 1427, 1400, 1434, 1434, 1434, 1420, 1413, 1420, 1407, 1420, 1393, 1441, 1372, 1393, 1365, 1420, 1372, 1365, 1358, 1393, 1365, 1365, 1331, 1358, 1331, 1358, 1317, 1331, 1358, 1262, 1069, 834, 910, 924, 889, 903, 834, 779, 752, 696, 676, 655, 621, 607, 621, 593, 586, 579, 517, 352];
 flow = [0.00, 0.12, 4.13, 6.10, 5.99, 5.94, 6.02, 5.94, 5.99, 6.10, 5.86, 6.03, 6.00, 5.94, 6.01, 6.06, 5.93, 6.01, 6.04, 5.98, 5.97, 5.94, 6.03, 5.99, 5.99, 5.99, 5.99, 6.06, 5.88, 6.07, 6.00, 5.90, 6.00, 6.01, 5.92, 6.07, 5.93, 6.10, 5.93, 5.97, 6.03, 5.90, 5.96, 6.05, 5.98, 6.03, 5.97, 6.01, 5.97, 5.96, 6.04, 6.01, 5.95, 5.97, 5.98, 6.00, 5.99, 5.33, 2.26, 0.62, 4.84, 6.01, 5.98, 6.00, 5.98, 5.98, 6.06, 5.97, 6.01, 5.98, 6.02, 5.98, 5.97, 6.01, 5.92, 6.10, 5.08, 0.98];
 
-% Visualize Data
-figure
+% Visualize Raw Data
+figure %pressure data
 subplot(2,1,1); 
     plot(time, pressure);
-    xline(5);
-    xline(15);
-    xline(17.077);
+    xline(16.12); %end of steady state by visual inspection
+                  %(around where part of preprocessing should occur)
     xlabel('time(sec)');
     ylabel('pressure(kPa)');
     title('Catheter Flow and Pressure Data');
     
-subplot(2,1,2); 
+subplot(2,1,2); % flow data
     plot(time, flow);
-    xline(5);
-    xline(15);
-    xline(17.077);
+    xline(16.12);
     xlabel('time(sec)');
     ylabel('flow rate(mL/s)');
+    
+% Visualize Raw Data's Derivative
+% Ricky: I thought it might be useful to use the derivative because I
+% expect the end of the first steady state to have a significant
+% discontinuity in the derivative.
+deriv_pressure = diff(pressure);
+deriv_pressure = [0 deriv_pressure];
+deriv_flow = diff(flow);
+deriv_flow = [0 deriv_flow];
 
-new_time = (time < 16);    % Focus on contrast phase to increase accuracy
-flow = flow(new_time);
-pressure = pressure(new_time);
-time = time(new_time);
-% Visualize data subset
-figure
+figure %pressure data
 subplot(2,1,1); 
-    plot(time, pressure);
+    plot(time,deriv_pressure);
+    xline(16.12); %end of steady state by visual inspection
+                  %(around where part of preprocessing should occur)
     xlabel('time(sec)');
-    ylabel('pressure(kPa)');
-    title('New Catheter Flow and Pressure Data');
-subplot(2,1,2); 
-    plot(time, flow);
+    ylabel('d/dt(pressure(kPa))');
+    title('Catheter Flow and Pressure Data');
+    
+subplot(2,1,2); % flow data
+    plot(time,deriv_flow);
+    xline(16.12);
     xlabel('time(sec)');
-    ylabel('flow rate(mL/s)');
-
+    ylabel('d/dt(flow rate(mL/s))');
