fb30436ea4233b0c580b2eff7b8e7c111388ddc7
Mia Han <miah@andrew.cmu.edu>
2020-04-30 20:42:49 -0400

    matlab functionality works -need to integrate with actual matlab data bc i dont hve matlab installed

--- gui/callMatlabInPython.py
+++ gui/callMatlabInPython.py
@@ -6,12 +6,21 @@
 
 #To use matlab to install the API, follow the instructions at:
 #https://www.mathworks.com/help/matlab/matlab_external/install-the-matlab-engine-for-python.html
-import matlab.engine
-eng = matlab.engine.start_matlab()
+# import matlab.engine
+# eng = matlab.engine.start_matlab()
 
-[time,pressure,flow,q_err] = eng.StepModel(nargout=4)
-print(time)
+# [time,pressure,flow,q_err] = eng.StepModel(nargout=4)
+# print(time)
 
 #Note, the way I got this to work is to have both the python and matlab functions
 #That you will be using all in the same folder -> this will make it easier to run
 #Whatever your model outputs in Python
+
+def retrieveMatLab():
+	time = list(range(0, 10))
+	pressure = list(range(0, 100,10))
+	flow = list(range(5, 55, 5))
+	q_err = list(range(0,50, 5))
+	# eng = matlab.engine.start_matlab()
+	# time,pressure,flow,q_err = eng.StepModel(nargout=4)
+	return time, pressure, flow, q_err
--- gui/test.py
+++ gui/test.py
@@ -6,6 +6,7 @@ import sys
 from datetime import datetime
 import bmeGUI
 import pyqtgraph as pg
+import callMatlabInPython
 
 class ExampleApp(QtWidgets.QMainWindow, bmeGUI.Ui_MainWindow):
 	def __init__(self, parent=None):
@@ -13,18 +14,62 @@ class ExampleApp(QtWidgets.QMainWindow, bmeGUI.Ui_MainWindow):
 		self.setupUi(self)
 		self.simulateNN.clicked.connect(self.simulate_neuralNet)
 		self.simulateMathModel.clicked.connect(self.simulate_mathModel)
-		
+		self.runMatLabModel.clicked.connect(self.start_MatLabModel)
+		self.displayPressureGraph.clicked.connect(self.update_graph_pressure)
+		self.displayFlowGraph.clicked.connect(self.update_graph_flow)
+		self.displayQerr.clicked.connect(self.update_graph_q_err)
+		self.displayPressureGraph.setEnabled(False)
+		self.displayFlowGraph.setEnabled(False)
+		self.displayQerr.setEnabled(False)
+		self.time, self.pressure, self.flow, self.q_err = [], [], [], []
+
+		self.data = []
 
 	def simulate_neuralNet(self):
 		print('in simulate_neuralnet')
+		self.stackedWidget.setCurrentIndex(0)
 		current_time = datetime.now().strftime("%H:%M:%S")
-		self.console_log.setPlainText(current_time+": "+ "Start Neural Network simulation\n" + self.console_log.toPlainText())
-
+		self.console_log.setPlainText(current_time+": "+ "Switching to Neural Network Simulation\n" + self.console_log.toPlainText())
 
 	def simulate_mathModel(self):
 		print('in simulate_mathModel')
+		self.stackedWidget.setCurrentIndex(1)
 		current_time = datetime.now().strftime("%H:%M:%S")
-		self.console_log.setPlainText(current_time+": "+ "Start Math Model simulation \n" + self.console_log.toPlainText())
+		self.console_log.setPlainText(current_time+": "+ "Switching to MatLab Simulation\n" + self.console_log.toPlainText())
+
+	def start_MatLabModel(self):
+		self.setupGraph()
+		current_time = datetime.now().strftime("%H:%M:%S")
+		self.console_log.setPlainText(current_time+": "+ "Running MatLab Model\n" + self.console_log.toPlainText())
+		# function that retrieves mat lab data from callMatlabInPython is here
+		self.time, self.pressure, self.flow, self.q_err = callMatlabInPython.retrieveMatLab()
+		# wait for the matlab function to finish calculating the values
+		while self.time is None or self.pressure is None or self.flow is None or self.q_err is None:
+			pass
+		self.displayPressureGraph.setEnabled(True)
+		self.displayFlowGraph.setEnabled(True)
+		self.displayQerr.setEnabled(True)
+
+
+	def setupGraph(self):
+		self.lineGraphView.setBackground('w')
+		self.pen = pg.mkPen(color=(0,0,255))
+		self.lineGraphView.setLabel('left', 'Time')
+
+	def update_graph_pressure(self):
+		self.lineGraphView.clear()
+		self.lineGraphView.setLabel('bottom', 'Pressure')
+		self.lineGraphView.plot(self.time, self.pressure, pen=self.pen)
+
+	def update_graph_flow(self):
+		self.lineGraphView.clear()
+		self.lineGraphView.setLabel('bottom', 'Flow')
+		self.lineGraphView.plot(self.time,self.flow, pen=self.pen)
+
+	def update_graph_q_err(self):
+		self.lineGraphView.clear()
+		self.lineGraphView.setLabel('bottom', 'Q_err')
+		self.lineGraphView.plot(self.time,self.q_err, pen=self.pen)
 
 
 def main():
